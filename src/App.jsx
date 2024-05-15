e300f447a4698fbbb3375df8c32406d7a9214261
Alok Gupta <alokgupta1560@gmail.com>
2024-05-15 19:27:31 +0530

    Added quotes components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Quotes from "./components/Quotes";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="quote-container">
-      <h2 className="quote">
-        "Programming is not about what you know; it's about what you can figure
-        out."
-      </h2>
-      <p className="author">- Unknown</p>
-      <h2 className="quote2">
-        "The only way to learn a new programming language is by writing programs
-        in it."
-      </h2>
-      <p className="author">- Dennis Ritchie</p>
-    </div>
+    <>
+        <Quotes />
+    </>
   );
 }
 
---
+++ src/components/Quotes.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Quotes() {
+  return (
+    <div>
+      <div className="quote-container">
+        <h2 className="quote">
+          "Programming is not about what you know; it's about what you can
+          figure out."
+        </h2>
+        <p className="author">- Unknown</p>
+        <h2 className="quote2">
+          "The only way to learn a new programming language is by writing
+          programs in it."
+        </h2>
+        <p className="author">- Dennis Ritchie</p>
+
+        <h2 className="quotes3">
+          "The best error message is the one that never shows up."
+        </h2>
+        <p className="author">- Thomas Fuchs</p>
+        <h2 className="quotes4">
+          "The most disastrous thing that you can ever learn is your first
+          programming language."
+        </h2>
+        <p className="author">- Alan Kay</p>
+      </div>
+    </div>
+  );
+}
+
+export default Quotes;
